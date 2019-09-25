e16556b554e1025549076f46a59264ed6999d3c1
Peter Butzhammer <peter@butzhammer.de>
2019-09-25 21:30:17 +0200

    Extend tests with continuous input

--- Convolutional.Logic.Tests/Decoding/ContinuousInput.cs
+++ Convolutional.Logic.Tests/Decoding/ContinuousInput.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Convolutional.Logic.Extensions;
+using Convolutional.Logic.Scoring;
 using Shouldly;
 using Xunit;
 
@@ -10,20 +11,28 @@ namespace Convolutional.Logic.Tests.Decoding
 {
     public class ContinuousInput
     {
-        [Fact]
-        public void HamingDistance()
+        [Theory]
+        [MemberData(nameof(GetCases))]
+        public void TestExample(Func<IEnumerable<bool>, IEnumerable<double>, double> calcScore, ScoreMethod scoreMethod, double falseValue = 0.0)
         {
             var input = "11 10 00 01 01 11 00"
                 .ParseBools()
-                .Select(b => b?1.0: 0.0);
+                .Select(b => b?1.0: falseValue);
 
             var  expected = "1 0 1 1 0 0 0";
 
-            var decoder = new Viterbi<double>(CodeConfig.Size3_7_5.EnumerateTransitions(), HammingDistance.Calculate, ScoreMethod.Minimize);
+            var decoder = new Viterbi<double>(CodeConfig.Size3_7_5.EnumerateTransitions(), calcScore, scoreMethod);
             var res = decoder.Solve(input);
 
             res.ShouldBe(expected.ParseBools());
         }
 
+
+        public static IEnumerable<object[]> GetCases()
+        {
+            yield return new object[]{ (Func<IEnumerable<bool>, IEnumerable<double>, double>)HammingDistance.Calculate,  ScoreMethod.Minimize};
+            yield return new object[]{ (Func<IEnumerable<bool>, IEnumerable<double>, double>)new LogisticFunction().CalculateScore,  ScoreMethod.Maximize};
+            yield return new object[]{ (Func<IEnumerable<bool>, IEnumerable<double>, double>)new SymetricScore().CalculateScore,  ScoreMethod.Maximize, -1.0};
+        }
     }
 }
