4d81e63b404333892ba2e8c8bbe2e9a89b537554
Peter Butzhammer <peter@butzhammer.de>
2019-11-12 17:09:31 +0100

    Make indices of BlockInterleaver public readonly properties

--- Convolutional.Logic/Interleaver/BlockInterleaver.cs
+++ Convolutional.Logic/Interleaver/BlockInterleaver.cs
@@ -1,33 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Convolutional.Logic.Extensions;
+using System.Linq;
 
 namespace Convolutional.Logic.Interleaver
 {
     public class BlockInterleaver
     {
-        private readonly IReadOnlyList<int> indicesInterleave;
-        private readonly IReadOnlyList<int> indicesDeinterleave;
         private readonly int blockSize;
 
-        public BlockInterleaver(int blockSize, int seed = 0):this(new Random(seed).Indices(blockSize))
+        public BlockInterleaver(int blockSize, int seed = 0) : this(new Random(seed).Indices(blockSize))
         {
         }
 
+
         public BlockInterleaver(IEnumerable<int> indicesInterleave)
         {
-            this.indicesInterleave = indicesInterleave.ToArray();
-            blockSize = this.indicesInterleave.Count;
-            
-            if (this.indicesInterleave.Any(i => i >= blockSize) || this.indicesInterleave.NonUnique().Any())
-                    throw new ArgumentException("The interleave indices must be an array containing n distinct values from 0 to n-1.", nameof(indicesInterleave));
+            IndicesInterleave = indicesInterleave.ToArray();
+            blockSize = IndicesInterleave.Count;
 
-            var reverse = new int[this.blockSize];
+            if (IndicesInterleave.Any(i => i >= blockSize) || IndicesInterleave.NonUnique().Any())
+                throw new ArgumentException("The interleave indices must be an array containing n distinct values from 0 to n-1.", nameof(indicesInterleave));
+
+            var reverse = new int[blockSize];
             for (var i = 0; i < reverse.Length; i++)
-                reverse[this.indicesInterleave[i]] = i;
+                reverse[IndicesInterleave[i]] = i;
 
-            indicesDeinterleave = reverse;
+            IndicesDeinterleave = reverse;
+        }
+
+        public IReadOnlyList<int> IndicesDeinterleave { get; }
+        public IReadOnlyList<int> IndicesInterleave { get; }
+
+        public IReadOnlyList<T> Deinterleave<T>(IReadOnlyList<T> input)
+        {
+            var res = new T[blockSize];
+            for (var i = 0; i < res.Length; i++)
+                res[i] = input[IndicesDeinterleave[i]];
+
+            return res;
         }
 
         public IReadOnlyList<T> Interleave<T>(IReadOnlyList<T> input)
@@ -35,8 +46,8 @@ namespace Convolutional.Logic.Interleaver
             AssertInputSize(input);
 
             var res = new T[blockSize];
-            for (var i = 0; i < res.Length; i++) 
-                res[i] = input[indicesInterleave[i]];
+            for (var i = 0; i < res.Length; i++)
+                res[i] = input[IndicesInterleave[i]];
 
             return res;
         }
@@ -46,15 +57,5 @@ namespace Convolutional.Logic.Interleaver
             if (input.Count != blockSize)
                 throw new InvalidOperationException($"This Interleaver can only handle inputs with {blockSize} elements. Got input with {input.Count} elements.");
         }
-
-        public IReadOnlyList<T> Deinterleave<T>(IReadOnlyList<T> input)
-        {
-            var res = new T[blockSize];
-            for (var i = 0; i < res.Length; i++)
-                res[i] = input[indicesDeinterleave[i]];
-
-            return res;
-        }
-
     }
 }
